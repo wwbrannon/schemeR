0f72d585a14deee00dc2e65c1e888e646401e2dc
Will Brannon <will.brannon@gmail.com>
2017-01-01 22:36:42 +0000

    Stubs for more things to implement

---
+++ R/functions.R
@@ -0,0 +1,4 @@
+## Various basic Scheme functions implemented for R. Unlike the versions in
+## the "lisp" package, these work on both lists and vectors, and include more
+## functions.
+
--- R/macro.R
+++ R/macro.R
@@ -1,23 +1,2 @@
-## A Lisp-style macro facility for R
-
-#' @export
-. <-
-function(f, ...)
-{
-    match.call()[-1]
-}
-
-reval <-
-function(expr)
-{
-
-}
-
-#' @export
-macroProcess <-
-function(files)
-{
-    filename <- 'R/foo.Rl'
-    txt <- readChar(filename, file.info(filename)$size)
-}
+## A Lisp-like macro facility for R
 
