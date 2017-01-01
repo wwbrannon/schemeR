882a670f73411a1f741731b04637d580f68aca4c
Will Brannon <will.brannon@gmail.com>
2017-01-01 22:44:04 +0000

    Implementing some basic scheme functions

--- R/functions.R
+++ R/functions.R
@@ -2,3 +2,37 @@
 ## the "lisp" package, these work on both lists and vectors, and include more
 ## functions.
 
+lambda <- `function`
+define <- `<-`
+map <- Map
+display <- print
+cons <- c
+
+nil <- NULL
+
+first <-
+function(obj)
+{
+    if(is.list(obj) || is.pairlist(obj))
+    {
+        return(obj[[1]])
+    } else
+    {
+        return(obj[1])
+    }
+}
+
+last <-
+function(obj)
+{
+    n <- length(obj)
+
+    if(is.list(obj) || is.pairlist(obj))
+    {
+        return(obj[[n]])
+    } else
+    {
+        return(obj[n])
+    }
+}
+
