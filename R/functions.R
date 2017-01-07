56a03ec03bb28fb39e802fdac110e2322819a2d5
William Brannon <will.brannon@gmail.com>
2017-01-07 09:07:30 -0500

    the c____r family of list-access functions and misc

--- R/functions.R
+++ R/functions.R
@@ -1,16 +1,20 @@
-## Various basic Scheme functions implemented for R. Unlike the versions in
-## the "lisp" package, these work on both lists and vectors, and include more
-## functions.
+### Various basic Scheme functions implemented for R. Unlike the versions in
+### the "lisp" package, these work on both lists and vectors, and include more
+### functions. Semantics are not always the same as in classic Scheme (e.g.,
+### the empty list is not nil, because in R, is.null(pairlist()) but not
+### is.null(list())).
 
+## Aliases for things already available in R under other names
+nil <- NULL
 lambda <- `function`
 define <- `<-`
 map <- Map
 display <- print
 cons <- c
+is.nil <- is.null
 
-nil <- NULL
-
-first <-
+## Type-generic list- or vector-processing functions
+car <-
 function(obj)
 {
     if(is.list(obj) || is.pairlist(obj))
@@ -20,6 +24,13 @@ function(obj)
     {
         return(obj[1])
     }
+}
+
+cdr <-
+function(lst)
+{
+    #Valid for both lists and vectors
+    return(lst[-1])
 }
 
 last <-
@@ -35,4 +46,41 @@ function(obj)
         return(obj[n])
     }
 }
+
+first <- car
+caar <- function(lst) { return(car(car(lst))) }
+cadr <- function(lst) { return(car(cdr(lst))) }
+cdar <- function(lst) { return(cdr(car(lst))) }
+cddr <- function(lst) { return(cdr(cdr(lst))) }
+caaar <- function(lst) { return(car(car(car(lst)))) }
+caadr <- function(lst) { return(car(car(cdr(lst)))) }
+cadar <- function(lst) { return(car(cdr(car(lst)))) }
+caddr <- function(lst) { return(car(cdr(cdr(lst)))) }
+cdaar <- function(lst) { return(cdr(car(car(lst)))) }
+cdadr <- function(lst) { return(cdr(car(cdr(lst)))) }
+cddar <- function(lst) { return(cdr(cdr(car(lst)))) }
+cdddr <- function(lst) { return(cdr(cdr(cdr(lst)))) }
+caaaar <- function(lst) { return(car(car(car(car(lst))))) }
+caaadr <- function(lst) { return(car(car(car(cdr(lst))))) }
+caadar <- function(lst) { return(car(car(cdr(car(lst))))) }
+caaddr <- function(lst) { return(car(car(cdr(cdr(lst))))) }
+cadaar <- function(lst) { return(car(cdr(car(car(lst))))) }
+cadadr <- function(lst) { return(car(cdr(car(cdr(lst))))) }
+caddar <- function(lst) { return(car(cdr(cdr(car(lst))))) }
+cadddr <- function(lst) { return(car(cdr(cdr(cdr(lst))))) }
+cdaaar <- function(lst) { return(cdr(car(car(car(lst))))) }
+cdaadr <- function(lst) { return(cdr(car(car(cdr(lst))))) }
+cdadar <- function(lst) { return(cdr(car(cdr(car(lst))))) }
+cdaddr <- function(lst) { return(cdr(car(cdr(cdr(lst))))) }
+cddaar <- function(lst) { return(cdr(cdr(car(car(lst))))) }
+cddadr <- function(lst) { return(cdr(cdr(car(cdr(lst))))) }
+cdddar <- function(lst) { return(cdr(cdr(cdr(car(lst))))) }
+cddddr <- function(lst) { return(cdr(cdr(cdr(cdr(lst))))) }
+
+
+
+
+
+
+
 
