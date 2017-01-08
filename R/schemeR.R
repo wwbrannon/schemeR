bc71a6dce4e6b54027a7eeba12a28e2de4e34f07
William Brannon <will.brannon@gmail.com>
2017-01-08 18:03:36 -0500

    Documentation updates

--- R/schemeR.R
+++ R/schemeR.R
@@ -1,36 +1,104 @@
-#' schemeR: Ports of certain Scheme facilities to R
+#' schemeR: Ports of Scheme features to R
 #'
-#' The schemeR package provides R versions of certain language features found
-#' in "proper" Scheme dialects, insofar as this is possible without modifying
-#' R itself. In particular, two categories of features are supported: a prefix
-#' syntax for R code, like the \code{(car (cdr (cddr x)))} syntax in Lisp
-#' dialects, and a way to write Lisp-like macros that are optimized to work
-#' with this sort of prefix-formatted code.
+#' Provides a way to write R code in a Lisp-like \code{\link{prefix}} form,
+#' with access to versions of many common Lisp operators (let, letrec, cond,
+#' do, etc.). True Lisp macros are also supported and are optimized to work
+#' with this sort of prefix code.
+#'
+#' @section Interface functions:
+#' The main entry point is the \code{schemeR()} function, which translates
+#' prefix code into the usual infix R form, and evaluates it. Other functions
+#' involved in implementing the prefix syntax are \code{prefix()} and
+#' \code{infix()}, which respectively translate infix code \strong{to} prefix,
+#' and translate prefix code \strong{to} infix. The \code{.} function is the
+#' building block of prefix code, but is not especially useful in its own
+#' right.
+#'
+#' @section Lisp operators:
+#' A large number of Lisp operators have been implemented, mostly in ways that
+#' are useful in either prefix or infix code. The highlight is the reasonably
+#' complete \code{\link{macro}} support, but other operators run the gamut from
+#' functional programming primitives (\code{lambda}, \code{zip},
+#' \code{for.each}, etc.) to list/vector manipulation (\code{car}, \code{cdr},
+#' \code{set.car}, etc.) to block-structure and lexical binding operators
+#' (\code{let}, \code{letrec}, \code{cond}, etc.). See \code{\link{functions}}
+#' for a full list and discussion.
+#'
+#' @docType package
+#' @name schemeR-package
+NULL
+
+#' Write R code in a Lisp-like form
+#'
+#' schemeR() allows embedding Scheme-like code written in \code{\link{prefix}}
+#' format, with the option of using various Lisp operators and features
+#' including macros, into R code. The code in question is still syntactically
+#' valid R, but has to be converted from \code{\link{prefix}} to
+#' \code{\link{infix}} to be readily executable. Passing it to schemeR does the
+#' conversion and evaluates the resulting 'traditional' R code, with some control
+#' over the environment in which the evaluation is done.
+#'
+#' @param expr An expression, usually a block expression between "\{" and "\}",
+#' intended to be in \code{\link{prefix}} form.
+#'
+#' @param pkg If \code{FALSE}, call \code{eval()} with its defaults,
+#' \code{envir=parent.frame()} and \code{enclos=baseenv()}. If \code{TRUE},
+#' use \code{envir=parent.frame()} but \code{enclos=getNamespace("schemeR")}.
+#' Note in particular that because the package namespace environment descends
+#' from baseenv() and the base package namespace environment, base functions
+#' will still be accessible regardless of what environments the parent frame
+#' descends from. The motivation for this odd semantics is to allow expr to
+#' refer to the many exported functions that are standard in Lisp/Scheme
+#' (let, letrec, do, cond, etc.) without having to either use the \code{::}
+#' operator (which goes against the spirit of Lisp-like prefix code) or
+#' \code{require()} the package, as would, for example, be good practice
+#' when developing another package.
+#'
+#' @return The result of evaluating \code{infix(expr)} in the specified
+#' environment.
 #'
 #' @section Note:
-#' Because R is a Scheme dialect under the hood, prefix-formatted R is still
+#' Because R is a Scheme dialect under the hood, prefix or Lisp-like R is still
 #' valid R code and parses with the built-in R parser. All of R's rules about
 #' syntactic names (see \code{\link{make.names}}) still apply, and these rules
-#' are stricter than is usual in Scheme.
+#' are stricter than usual among Lisps.
 #'
 #' @section Limitations:
 #' Because our prefix syntax is just a preprocessing step (R code expressed
-#' with the \code{\link{.}}) function is transformed into its usual infix
+#' with the \code{\link{.}} function is transformed into its usual infix
 #' form before evaluation), R's function-call mechanism is still used exactly
-#' as-is. In particular, tail call elimination is still not possible, though
-#' it may be added to future versions of this package as something the user
-#' can request for prefix functions.
+#' as-is. In particular, it's still not possible to do tail call elimination
+#' safely for arbitrary functions.
 #'
-#' @docType package
-#' @name schemeR
-NULL
-
-#' @rdname schemeR
+#' @examples
+#' schemeR::schemeR({
+#' .(let, .(x, .(c, 1, 3, 5, 7, 9)),
+#'   .(do, .(.(x, .(c, 1, 3, 5, 7, 9), .(cdr, x)),
+#'           .(s, 0, .(`+`, s, .(car, x))),
+#'           .(foo, 4)),
+#'     .(.(is.nil, x), s),
+#'     T))
+#' }, pkg=TRUE) #=> 25
+#'
+#' require(schemeR)
+#' schemeR({
+#' .(define, x, .(sort, .(sample, .(`:`, 1, 10), 5, replace=TRUE)))
+#' .(`for`, i, x, .(print, .(`:`, 1, i)))
+#' })
+#'
 #' @export
 schemeR <-
-function(expr)
+function(expr, pkg=FALSE)
 {
+    #TODO: we have to do recursive macro expansion here
+
     expr <- substitute(expr)
 
-    eval(infix(expr))
+    envir <- parent.frame()
+    if(!is.null(pkg) && pkg)
+        enclos <- getNamespace("schemeR")
+    else
+        enclos <- baseenv()
+
+    eval(infix(expr), envir=envir, enclos=enclos)
 }
