420d737e84e8faaccb3705a91aa0dd8831dab06c
William Brannon <will.brannon@gmail.com>
2017-01-09 20:27:16 -0500

    Doc updates and correcting various commands: let and friends take one list of bindings, and many commands provide implicit begin

--- R/flow-control.R
+++ R/flow-control.R
@@ -1,4 +1,44 @@
 ## Flow-control operators
+
+#' Flow-control operators
+#'
+#' These functions provide R versions of several Scheme flow-control operators.
+#' \code{cond()} and \code{case()} are conditional forms, \code{do()} is an
+#' iteration construct, and \code{or()} and \code{and()} allow for conditional
+#' short-circuit evaluation. See the vignettes for full details and a more
+#' in-depth discussion of how to use these operators.
+#'
+#' @param ... The infix form of prefix arguments. Note that for \code{and()}
+#' and \code{or()}, each element of ... is just an arbitrary R expression.
+#'
+#' @return
+#' Return values are different for different operators.
+#' \itemize{
+#' \item{or returns the value of the first expression which when
+#' coerced to logical is TRUE, or otherwise returns FALSE;}
+#' \item{and returns the value of the first expression which when
+#' coerced to logical is FALSE, or otherwise returns TRUE;}
+#' \item{case returns the value of the expression associated with its key
+#' expression's value, or if the key's value was not in any clause, returns
+#' NULL;}
+#' \item{cond returns the value of the last expression in the list of them
+#' associated with the first clause whose test condition was logically TRUE,
+#' or if no condition was TRUE, returns NULL;}
+#' \item{do returns the value of the expression provided with its test
+#' expression on the first iteration on which the test expression is logically
+#' TRUE (if no second expression was provided, the value of the test expression
+#' is returned).}
+#' }
+#'
+#' @examples
+#' # the print("foo") clause will never be reached in either expression
+#' schemeR::schemeR({
+#' .(or, 1==2, TRUE, print("foo"))
+#' .(and, 1==1, 3 > 4, print("foo"))
+#'  }, pkg=TRUE)
+#'
+#' @rdname flow-control
+#' @name flow-control
 #' @export
 or <-
 function(...)
@@ -19,6 +59,7 @@ function(...)
     return(ret)
 }
 
+#' @rdname flow-control
 #' @export
 and <-
 function(...)
@@ -39,9 +80,13 @@ function(...)
     return(ret)
 }
 
-#.(case, .(`+`, 1, 1),
-#  .(3, .(print, "foo")),
-#  .(2, .(print, "bar")))
+#' @examples
+#' schemeR::schemeR({
+#' .(case, .(`+`, 1, 1),
+#'   .(3, .(print, "foo")),
+#'   .(2, .(print, "bar")))
+#'  }, pkg=TRUE)
+#' @rdname flow-control
 #' @export
 case <-
 function(...)
@@ -71,10 +116,14 @@ function(...)
     return(invisible(NULL))
 }
 
-#.(cond,
-#  .(.(`==`, .(`+`, 1, 4), 4), .(print, "foo")),
-#  .(.(`==`, .(`+`, 1, 3), 5), .(print, "bar")),
-#  .(TRUE, .(print, "baz")))
+#' @examples
+#' schemeR::schemeR({
+#' .(cond,
+#'   .(.(`==`, .(`+`, 1, 4), 4), .(print, "foo")),
+#'   .(.(`==`, .(`+`, 1, 3), 5), .(print, "bar")),
+#'   .(TRUE, .(print, "baz")))
+#'  }, pkg=TRUE)
+#' @rdname flow-control
 #' @export
 cond <-
 function(...)
@@ -85,47 +134,53 @@ function(...)
     {
         if(eval(clause[[1]]))
         {
-            return(eval(clause[[2]]))
+            body <- do.call(expression, as.list(clause[2:length(clause)]))
+            return(eval(body))
         }
     }
 
     return(invisible(NULL))
 }
 
-#.(let, .(x, .(c, 1, 3, 5, 7, 9)),
-#  .(do, .(.(x, .(c, 1, 3, 5, 7, 9), .(cdr, x)),
-#          .(s, 0, .(`+`, s, .(car, x))),
-#          .(foo, 4)),
-#    .(.(is.nil, x), s),
-#    T)) #=> 25
+#' @examples
+#' schemeR::schemeR({
+#' .(let, .(.(x, .(c, 1, 3, 5, 7, 9))),
+#'   .(do, .(.(x, .(c, 1, 3, 5, 7, 9), .(cdr, x)),
+#'           .(s, 0, .(`+`, s, .(car, x))),
+#'           .(foo, 4)),
+#'     .(.(is.nil, x), s),
+#'     TRUE)) #=> 25
+#'  }, pkg=TRUE)
+#' @rdname flow-control
 #' @export
 do <-
 function(...)
 {
     args <- eval(substitute(alist(...)))
 
-    if(!(length(args) %in% c(2,3)))
-        stop("Incorrect number of arguments to do")
+    if(length(args) <= 1)
+        stop("Too few arguments to do")
 
     #The test-is-false loop body
-    if(length(args) == 3)
+    if(length(args) >= 3)
     {
-        cmd <- args[[length(args)]]
-        args <- args[1:(length(args) - 1)]
+        cmd <- do.call(expression, args[3:length(args)])
     } else
     {
         cmd <- quote(NULL)
     }
 
     #The test clause
-    tc <- args[[length(args)]]
-    if(!(length(tc) %in% c(1,2)))
+    tc <- args[[2]]
+    if(length(tc) == 0)
         stop("Invalid test clause for do")
 
-    args <- args[1:(length(args) - 1)]
-
     test <- tc[[1]]
-    test_expr <- ifelse(length(tc) == 2, tc[[2]], NULL)
+    test_expr <- ifelse(length(tc) >= 2,
+                        do.call(expression, as.list(tc[2:length(tc)])),
+                        NULL)
+
+    args <- args[1:1]
 
     #The bindings and step expressions. If no step expression is given for
     #a variable "x", make "x" the step expression - it's a no-op that makes
--- R/lexical.R
+++ R/lexical.R
@@ -1,18 +1,57 @@
 ## Lexical binding
 
-#FIXME: the bindings should all be under one list
 #FIXME: better / more function-specific examples
-#FIXME: various operators should take multiple expressions rather than just one
 
+#' Lexical binding constructs
+#'
+#' These functions provide the let, let* and letrec block-scope operators
+#' from Scheme. All three of them create temporary variables with specified
+#' values, successively evaluate provided body expressions, and return the
+#' value of the last expression.
+#'
+#' All three forms are syntactically identical:
+#'
+#' \code{
+#' .(LET_OP, .(.(variable, init) ...), expr, expr ...)
+#' }
+#'
+#' where LET_OP is \code{let}, \code{let.star} or \code{letrec}.
+#'
+#' They differ in the scope in which the temporary bindings (of names to init
+#' expressions' values) exist:
+#' \itemize{
+#' \item{let computes the init expressions before creating any bindings.}
+#' \item{let.star creates the bindings, and in the process evaluates the init
+#' expressions, sequentially left to right, so that earlier bindings are in
+#' scope for evaluating later init expressions.}
+#' \item{letrec evaluates all init expressions simultaneously, with all
+#' bindings in scope during evaluation. (The init expressions can therefore
+#' be mutually recursive.)}
+#' }
+#'
+#' @section Note:
+#' R, unlike Scheme, does not have a notion of variable definition separate
+#' from assignment. So the init expressions are mandatory here, whereas in
+#' Scheme it is possible to create uninitialized variables in the scope of the
+#' body block.
+#'
+#' The let, let.star and letrec functions take arguments in a way that seems
+#' natural in \code{\link{prefix}} notation, but makes for odd-looking calls
+#' in the usual infix form. Calling them "directly" - i.e., other than through
+#' prefix code and SchemeR() or infix() - is not recommended.
+#'
+#' @param ... The infix form of prefix arguments.
+#'
+#' @return The result of evaluating the final body expression.
 #'
 #' @examples
 #' schemeR::schemeR({
-#'.(let,
-#'    .(i, 3),
-#'    .(foo, 5),
-#'  .(`==`, i, .(`-`, foo, 2)))
+#'.(let, .(.(i, 3),
+#'         .(foo, 5)),
+#'    .(`==`, i, .(`-`, foo, 2)))
 #'  }, pkg=TRUE)
 #' @rdname lexical
+#' @name lexical
 #' @export
 let <-
 function(...)
@@ -22,30 +61,28 @@ function(...)
     if(length(args) <= 1)
         stop("Too few arguments to let")
 
-    for(a in args[1:(length(args) - 1)])
-        if(length(a) != 2)
+    for(b in as.list(args[[1]]))
+        if(length(b) != 2)
             stop("Invalid let binding")
 
-    #The statement we want to evaluate
-    body <- args[[length(args)]]
-    args <- args[1:(length(args) - 1)]
+    #Break it out into bindings and body exps
+    bindings <- args[[1]]
+    body <- do.call(expression, args[2:length(args)]) #the implicit progn
 
     #We need to evaluate all the inits before setting any of the variables;
     #this is not necessarily the same as evaluating them all with parent
     #parent.frame() rather than env, because they may have side effects.
-    vals <- lapply(args, function(x) eval(x[[2]], envir=parent.frame()))
-    names(vals) <- vapply(args, function(x) as.character(x[[1]]), character(1))
+    vals <- lapply(bindings, function(x) eval(x[[2]], envir=parent.frame()))
+    names(vals) <- vapply(bindings, function(x) as.character(x[[1]]), character(1))
 
     eval(body, envir=vals, enclos=parent.frame())
 }
 
-#'
 #' @examples
 #' schemeR::schemeR({
-#' .(let.star,
-#'     .(i, 3),
-#'     .(foo, 5),
-#'   .(`==`, i, .(`-`, foo, 2)))
+#' .(let.star, .(.(i, 3), .(foo, 5)),
+#'       .(`==`, i,
+#'               .(`-`, foo, 2)))
 #'  }, pkg=TRUE)
 #' @rdname lexical
 #' @export
@@ -57,20 +94,20 @@ function(...)
     if(length(args) <= 1)
         stop("Too few arguments to let.star")
 
-    for(a in args[1:(length(args) - 1)])
-        if(length(a) != 2)
+    for(b in as.list(args[[1]]))
+        if(length(b) != 2)
             stop("Invalid let.star binding")
 
-    #The statement we want to evaluate
-    body <- args[[length(args)]]
-    args <- args[1:(length(args) - 1)]
+    #Break it out into bindings and body exps
+    bindings <- args[[1]]
+    body <- do.call(expression, args[2:length(args)]) #the implicit progn
 
     #We need to "evaluate the bindings sequentially from left to right"
     env <- new.env(parent=parent.frame())
-    for(arg in args)
+    for(bind in as.list(bindings))
     {
-        nm <- as.character(arg[[1]])
-        val <- eval(arg[[2]], envir=env)
+        nm <- as.character(bind[[1]])
+        val <- eval(bind[[2]], envir=env)
 
         assign(nm, val, envir=env) #visible for subsequent evaluations
     }
@@ -78,20 +115,18 @@ function(...)
     eval(body, envir=env)
 }
 
-#'
+#FIXME
 #' @examples
 #' schemeR::schemeR({
-#' .(letrec,
-#'     .(i, 3),
-#'     .(foo, 5),
-#'   .(`==`, i, .(`-`, foo, 2)))
+#' .(letrec, .(.(i, 3), .(foo, 5)),
+#'      .(`==`, i,
+#'              .(`-`, foo, 2)))
 #'  }, pkg=TRUE)
 #'
 #' schemeR::schemeR({
-#' .(letrec,
-#'     .(i, 3),
-#'     .(foo, .(lambda, .(n), .(`+`, n, 1))),
-#'   .(`==`, i, .(foo, 2)))
+#' .(letrec, .(.(i, 3),
+#'             .(foo, .(lambda, .(n), .(`+`, n, 1)))),
+#'      .(`==`, i, .(foo, 2)))
 #'  }, pkg=TRUE)
 #' @rdname lexical
 #' @export
@@ -103,22 +138,23 @@ function(...)
     if(length(args) <= 1)
         stop("Too few arguments to letrec")
 
-    for(a in args[1:(length(args) - 1)])
-        if(length(a) != 2)
+    for(b in as.list(args[[1]]))
+        if(length(b) != 2)
             stop("Invalid letrec binding")
 
-    #We should evaluate the last statement passed with the caller's context
-    #as environment - make this the body of the function built below
-    body <- args[[length(args)]]
-    args <- args[1:(length(args) - 1)]
+    #Break it out into bindings and body exps
+    bindings <- args[[1]]
+    body <- do.call(expression, args[2:length(args)]) #the implicit progn
 
     #The bindings become the formals of the generated function, because
     #function formals can be defined in a mutually recursive way
-    vals <- lapply(args, function(x) x[[2]])
-    names(vals) <- vapply(args, function(x) as.character(x[[1]]), character(1))
+    vals <- lapply(bindings, function(x) x[[2]])
+    names(vals) <- vapply(bindings, function(x) as.character(x[[1]]), character(1))
 
     fn <- eval(call("function", as.pairlist(vals), body), envir=parent.frame())
     environment(fn) <- parent.frame()
 
+    #Unlike in lambda, call it: fn only exists as a block scoping
+    #mechanism, we care about the return value
     return(fn())
 }
