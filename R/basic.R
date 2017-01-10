310830ef6e2701bc2333c32e26725716c21bafc4
William Brannon <will.brannon@gmail.com>
2017-01-10 12:50:27 -0500

    Document basic.R

--- R/basic.R
+++ R/basic.R
@@ -1,41 +1,160 @@
-## Some basic / misc renamings of things R already has
+#FIXME: remove the annoying auto-generated "Format" section from
+#basic-aliases.Rd before shipping to CRAN
+
+#' Lisp-friendly basic aliases
+#'
+#' These functions and other objects make things that are already available
+#' in base R available under their traditional names in Scheme and other Lisp
+#' dialects. See 'Details' for a full list.
+#'
+#' The aliases defined are:
+#' \itemize{
+#' \item{\code{display} is an alias for \code{print}}
+#' \item{\code{progn} is an alias for \code{\{}}
+#' \item{\code{begin} is an alias for \code{\{}}
+#' \item{\code{fromPkg} is an alias for \code{::}}
+#' \item{\code{is.nil} is an alias for \code{is.null}}
+#' \item{\code{is.procedure} is an alias for \code{is.function}}
+#' \item{\code{is.boolean} is an alias for \code{is.logical}}
+#' \item{\code{nil} is an alias for \code{NULL}}
+#' }
+#'
+#' See the respective help pages for the base names for more details.
+#'
+#' @param x An object.
+#' @param ... Further arguments to pass to aliased base R functions.
+#' @param pkg The package fromPkg should look in.
+#' @param name The name of the object fromPkg should look for.
+#'
+#' @return Return values are as for the aliased base functions.
+#'
+#' @rdname basic-aliases
+#' @name basic-aliases
+#' @export
+display <-
+function(x, ...)
+{
+    return(do.call(curry(print, x), list(...)))
+}
+
+#' @rdname basic-aliases
+#' @export
+progn <-
+function(...)
+{
+    lst <- c(list(as.symbol("{")), list(...))
+    return(as.call(lst))
+}
+
+#' @rdname basic-aliases
+#' @export
+begin <-
+function(...)
+{
+    lst <- c(list(as.symbol("{")), list(...))
+    return(as.call(lst))
+}
+
+#' @rdname basic-aliases
+#' @export
+fromPkg <-
+function(pkg, name)
+{
+    lst <- list(pkg=as.character(pkg), name=as.character(name))
+    return(do.call(`::`, lst))
+}
+
+#' @rdname basic-aliases
+#' @export
+is.nil <-
+function(x)
+{
+    return(is.null(x))
+}
+
+#' @rdname basic-aliases
+#' @export
+is.procedure <-
+function(x)
+{
+    return(is.function(x))
+}
+
+#' @rdname basic-aliases
+#' @export
+is.boolean <-
+function(x)
+{
+    return(is.logical(x))
+}
+
+#' @rdname basic-aliases
 #' @export
 nil <- NULL
 
+#' Non-binary comparison operators
+#'
+#' These functions provide versions of R's built-in binary comparison operators
+#' that work with arbitrarily many arguments. \code{ge} is the \code{>=}
+#' operator, \code{le} is \code{<=}, \code{eq} is \code{==}, \code{lt} is
+#' \code{<}, and \code{gt} is \code{>}.
+#'
+#' If more than 2 arguments are passed, the result is the logical conjunction
+#' of the binary operator's return value for each pair of arguments (in order).
+#' For example, le(1,2,3) is equivalent to 1 <= 2 <= 3 (mathematically
+#' speaking; the second of those is not a valid R expression). In R,
+#' le(1,2,3) is equivalent to 1 <= 2 && 2 <= 3, and so on for more arguments.
+#'
+#' @param ... Values to compare.
+#'
+#' @return The boolean result of the comparison.
+#'
+#' @examples
+#' le(1,2,3) == TRUE
+#'
+#' ge(1,2,3) == FALSE
+#'
+#' eq(1,1,1) == TRUE
+#'
+#' eq(1,2,3) == FALSE
+#'
+#' @rdname nonbinary-comparison
+#' @name nonbinary-comparison
 #' @export
-display <- print
+eq <-
+function(...)
+{
+    Reduce(`==`, list(...))
+}
 
+#' @rdname nonbinary-comparison
 #' @export
-progn <- `{`
+ge <-
+function(...)
+{
+    Reduce(`>=`, list(...))
+}
 
+#' @rdname nonbinary-comparison
 #' @export
-begin <- progn
+le <-
+function(...)
+{
+    Reduce(`<=`, list(...))
+}
 
+#' @rdname nonbinary-comparison
 #' @export
-fromPkg <- `::`
+gt <-
+function(...)
+{
+    Reduce(`>`, list(...))
+}
 
+#' @rdname nonbinary-comparison
 #' @export
-is.nil <- is.null
-
-#' @export
-is.procedure <- is.function
-
-#' @export
-is.boolean <- is.logical
-
-## Non-binary comparison operators
-## eq, ge, le, gt, lt: =, >=, <=, >, <
-#' @export
-eq <- function(...) { Reduce(`==`, list(...)) }
-
-#' @export
-ge <- function(...) { Reduce(`>=`, list(...)) }
-
-#' @export
-le <- function(...) { Reduce(`<=`, list(...)) }
-
-#' @export
-gt <- function(...) { Reduce(`>`, list(...)) }
-
-#' @export
-lt <- function(...) { Reduce(`<`, list(...)) }
+lt <-
+function(...)
+{
+    Reduce(`<`, list(...))
+}
