a5975132ada3dc549a6dbd5cc03e3d2b75d981c4
William Brannon <will.brannon@gmail.com>
2017-01-14 12:33:23 -0500

    Copy examples as stub tests for lexical.R

--- tests/testthat/test-lexical.R
+++ tests/testthat/test-lexical.R
@@ -1,13 +1,31 @@
 context("Lexical binding constructs")
 
 test_that("let works correctly", {
-
+    #schemeR::schemeR({
+    #.(let, .(.(i, 3),
+    #         .(foo, 5)),
+    #    .(`==`, i, .(`-`, foo, 2)))
+    #}, pkg=TRUE)
 })
 
 test_that("let.star works correctly", {
-
+    #schemeR::schemeR({
+    #.(let.star, .(.(i, 3), .(foo, 5)),
+    #           .(`==`, i,
+    #               .(`-`, foo, 2)))
+    #}, pkg=TRUE)
 })
 
 test_that("letrec works correctly", {
+    #schemeR::schemeR({
+    #.(letrec, .(.(i, 3), .(foo, 5)),
+    #          .(`==`, i,
+    #              .(`-`, foo, 2)))
+    #}, pkg=TRUE)
 
+    #schemeR::schemeR({
+    #.(letrec, .(.(i, 3),
+    #            .(foo, .(lambda, .(n), .(`+`, n, 1)))),
+    #          .(`==`, i, .(foo, 2)))
+    #}, pkg=TRUE)
 })
