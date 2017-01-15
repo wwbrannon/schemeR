e24acc90aa65be214783b45313b07658dfd01a72
William Brannon <will.brannon@gmail.com>
2017-01-14 19:02:38 -0500

    Update macro example - infix version works!

--- inst/examples/macros.R
+++ inst/examples/macros.R
@@ -9,26 +9,28 @@
 
 # A prefix version
 schemeR::schemeR({
-    .(defmacro, lcomp, .(exp, s1, var, s2, lst, conditional, test),
-        .(let, .(.(result, .(gensym, "G1")), .(lvar, .(gensym, "G2"))),
-            .b(.(let, .(.(.c(lvar), .c(lst))),
-                    .(do, .(.(.c(result), nil, .(append, .c(result), .c(exp))),
-                            .(.c(var), car(.c(lvar)), car(.c(lvar)))),
-                          .(.(conditional, .c(test)), .c(result)),
-                          .(set, .c(lvar), .(cdr, .c(lvar))))))))
+.(defmacro, lcomp, .(exp, s1, var, s2, lst, s3, test),
+    .(let, .(.(result, .(gensym, "G1")), .(lvar, .(gensym, "G2"))),
+        .b(.(let, .(.(.c(lvar), .c(lst))),
+                .(do, .(.(.c(result), nil, .(cond, .(.c(test), .(append, .c(result), .c(exp))),
+                                                   .(TRUE, .c(result)))),
+                        .(.c(var), car(.c(lvar)), car(.c(lvar)))),
+                      .(.(is.nil, .c(lvar)), .c(result)),
+                      .(set, .c(lvar), .(cdr, .c(lvar))))))))
 }, pkg=TRUE)
 
 # An infix version
 zls <- alist(x=)$x
 defmacro(lcomp, alist(exp=zls, s1=zls, var=zls, s2=zls, lst=zls,
-                      conditional=zls, test=zls),
+                      s3=zls, test=zls),
     let(list(list(result, gensym("G1")), list(lvar, gensym("G2"))),
         quasiquote({
             let(list(list(.c(lvar), .c(lst))),
                 do(list(
-                        list(.c(result), nil, append(.c(result), list(.c(exp)))),
+                        list(.c(result), nil, cond(list(.c(test), append(.c(result), list(.c(exp)))),
+                                                   list(TRUE, .c(result)))), #leave unchanged
                         list(.c(var), car(.c(lvar)), car(.c(lvar)))),
-                   list(do.call(.c(conditional), list(.c(test))), .c(result)),
+                   list(is.nil(.c(lvar)), .c(result)),
                    set(.c(lvar), cdr(.c(lvar)))))
         })
     )
